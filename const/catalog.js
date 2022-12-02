628f003c8942f73a0fa7de0f74fec470d8495c3e
schulya <96183796+Schuyaa@users.noreply.github.com>
2022-12-02 18:25:00 +0500

    Add files via upload

---
+++ const/catalog.js
@@ -0,0 +1,62 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: '«Happy Ranch»',
+        img: 'https://pufoff.ru/assets/images/products/1018/kreslo-meshok-happy-ranch.jpg',
+        price: 1990,
+    },
+    {
+        id: 'el2',
+        name: 'Бескаркасное кресло мешок «Cosmos»',
+        img: 'https://pufoff.ru/assets/images/products/165/kreslo-meshok-grusha-cosmos.jpg',
+        price: 2690,
+    },
+    {
+        id: 'el3',
+        name: 'Кресло-мешок «Morocco»',
+        img: 'https://pufoff.ru/assets/images/products/168/kreslo-meshok-morocco.jpg',
+        price: 2690,
+    },
+    {
+        id: 'el4',
+        name: '«Diamond»',
+        img: 'https://pufoff.ru/assets/images/products/171/kreslo-meshok-grusha-diamond.jpg',
+        price: 2690,
+    },
+    {
+        id: 'el5',
+        name: 'Кресло-груша «Lamas Grey»',
+        img: 'https://pufoff.ru/assets/images/products/744/kreslo-meshok-lamas-grey800.jpg',
+        price: 2990,
+    },
+    {
+        id: 'el6',
+        name: '«Cooper Dark Grey»',
+        img: 'https://pufoff.ru/assets/images/products/978/cooper-black-kreslo-grusha3.jpg',
+        price: 3090,
+    },
+    {
+        id: 'el7',
+        name: 'Кресло-мешок Papaya',
+        img: 'https://pufoff.ru/assets/images/products/835/kreslo-meshok-papaya.jpg',
+        price: 4990,
+    },
+    {
+        id: 'el8',
+        name: 'Sorgo',
+        img: 'https://pufoff.ru/assets/images/products/834/kreslo-meshok-sorgo.jpg',
+        price: 4990,
+    },
+    {
+        id: 'el9',
+        name: 'Papaya Color',
+        img: 'https://pufoff.ru/assets/images/products/836/kreslo-meshok-papaya-color.jpg',
+        price: 4990,
+    },
+    {
+        id: 'el10',
+        name: 'Кресло груша «Colibri»',
+        img: 'https://pufoff.ru/assets/images/products/1071/kreslo-meshok-grusha-colibri.jpg',
+        price: 4990,
+    },
+];
